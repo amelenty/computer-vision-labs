3f6a085ffd4e92b8f43f4e22d034c88826e0fee7
amelentyeva <amelentyeva@rubiconproject.com>
2018-11-04 11:08:58 +0200

    :sparkles: match other images with base

--- lab1/ada/lab1.py
+++ lab1/ada/lab1.py
@@ -17,10 +17,7 @@ def shrink_image(image):
     return image
 
 
-if __name__ == '__main__':
-    cv2.startWindowThread()
-    sift = cv2.xfeatures2d.SIFT_create()
-
+def process_base_image():
     # Read the image we'll use as a base, make it grayscale and shrink it
     # If it's not grayscale, OpenCV crashes.
     # If it's too big, OpenCV crashes without mentioning that it ran out of memory (but it did).
@@ -28,19 +25,47 @@ if __name__ == '__main__':
     base_gray = cv2.cvtColor(base, cv2.COLOR_BGR2GRAY)
     base_gray_small = shrink_image(base_gray)
     cv2.imshow("Base gray", base_gray_small)
-    cv2.waitKey(IMG_SHOW_DELAY_MS)
 
     # Find key points and compute the descriptor
     key_points, descriptor = sift.detectAndCompute(base_gray_small, None)
 
     # Show key points on image.
     # You have to pass the output image to cv2.drawKeyPoints, also you have to declare it first.
-    base_key_points = base_gray_small
-    base_key_points = cv2.drawKeypoints(base_gray_small, key_points, base_key_points)
-    base_key_points_rich = base_gray_small
-    base_key_points_rich = cv2.drawKeypoints(base_gray_small, key_points, base_key_points_rich,
+    base_key_points_img = base_gray_small
+    base_key_points_img = cv2.drawKeypoints(base_gray_small, key_points, base_key_points_img)
+    base_key_points_rich_img = base_gray_small
+    base_key_points_rich_img = cv2.drawKeypoints(base_gray_small, key_points, base_key_points_rich_img,
                                              flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    cv2.imshow("Base with key points", base_key_points)
-    cv2.waitKey(IMG_SHOW_DELAY_MS)
-    cv2.imwrite(OUTPUT_DIRECTORY + "base_key_points.jpg", base_key_points)
-    cv2.imwrite(OUTPUT_DIRECTORY + "base_key_points_rich.jpg", base_key_points_rich)
+    cv2.imshow("Base with key points", base_key_points_img)
+    cv2.imwrite(OUTPUT_DIRECTORY + "base_key_points.jpg", base_key_points_img)
+    cv2.imwrite(OUTPUT_DIRECTORY + "base_key_points_rich.jpg", base_key_points_rich_img)
+
+    return base_gray_small, key_points, descriptor
+
+
+def process_test_image(image, name):
+    image_gray_small = shrink_image(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))
+    key_points, descriptor = sift.detectAndCompute(image_gray_small, None)
+
+    # I used L1 because they say that "L1 and L2 norms are preferable choices for SIFT and SURF descriptors".
+    bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
+
+    # Query goes first, train goes second. Same with cv2.drawMatches().
+    matches = bf.match(descriptor, base_descriptor)
+    match_img = cv2.drawMatches(image_gray_small, key_points, base_image, base_key_points, matches, None)
+    cv2.imshow(name, match_img)
+
+
+if __name__ == '__main__':
+    cv2.startWindowThread()
+    sift = cv2.xfeatures2d.SIFT_create()
+
+    (base_image, base_key_points, base_descriptor) = process_base_image()
+
+    img_names = ['001+.jpg', '003-.jpg', '010+.jpg', '020+.jpg', '021-.jpg']
+
+    for img_name in img_names:
+        img = cv2.imread(DATA_DIRECTORY + img_name)
+        process_test_image(img, img_name)
+
+    cv2.waitKey()
