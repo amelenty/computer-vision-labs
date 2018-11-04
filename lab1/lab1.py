add BRISK, fix some bugs with image scaling and None values

lab1/lab1.py
@@ -6,38 +6,47 @@ import time
 import random
 from metric import Metric
 
-DATA_DIRECTORY_ADA = "resources/ada/"
-DATA_DIRECTORY_VLADA = "resources/vlada/"
-OUTPUT_DIRECTORY_ADA = "output/ada/"
-OUTPUT_DIRECTORY_VLADA = "output/vlada/"
+DATA_DIRECTORY = "resources/"
+OUTPUT_DIRECTORY = "output/"
 IMG_SHOW_DELAY_MS = 10000
+MAX_IMAGE_HEIGHT = 800
 MAX_IMAGE_WIDTH = 800
 MIN_MATCH_COUNT = 10
-IMAGE_SHOW_SAMPLING = 0.1
+IMAGE_SHOW_SAMPLING = 0.025
+COLOR_MAGENTA = (0xff, 0x00, 0xff)
 
 
 # Resize image so that width is MAX_IMAGE_WIDTH or less, keep ratio
 def shrink_image(image):
     # image is a NumPy ndarray, so image.shape is (height, width, depth)
     (height, width) = image.shape[:2]
+
     if width > MAX_IMAGE_WIDTH:
         ratio = MAX_IMAGE_WIDTH / width
         # new size passed to resize() should be (width, height)
-        return cv2.resize(image, (MAX_IMAGE_WIDTH, int(height * ratio)))
+        image = cv2.resize(image, (MAX_IMAGE_WIDTH, int(height * ratio)))
+        (height, width) = image.shape[:2]
+
+    if height > MAX_IMAGE_HEIGHT:
+        ratio = MAX_IMAGE_HEIGHT / height
+        # new size passed to resize() should be (width, height)
+        image = cv2.resize(image, (int(width * ratio), MAX_IMAGE_HEIGHT))
+
     return image
 
 
-def process_base_image_sift():
+# Performs either SIFT for Ada, or BRISK for Vlada
+def process_base_image(student, algo_name, algorithm):
     # Read the image we'll use as a base, make it grayscale and shrink it
     # If it's not grayscale, OpenCV crashes.
     # If it's too big, OpenCV crashes without mentioning that it ran out of memory (but it did).
-    base = cv2.imread(DATA_DIRECTORY_ADA + "base.jpg")
+    base = cv2.imread(DATA_DIRECTORY + student + "/base.jpg")
     base_gray = cv2.cvtColor(base, cv2.COLOR_BGR2GRAY)
     base_gray_small = shrink_image(base_gray)
     cv2.imshow("Base gray", base_gray_small)
 
     # Find key points and compute the descriptor
-    key_points, descriptor = sift.detectAndCompute(base_gray_small, None)
+    key_points, descriptor = algorithm.detectAndCompute(base_gray_small, None)
 
     # Show key points on image.
     # You have to pass the output image to cv2.drawKeyPoints, also you have to declare it first.
@@ -46,9 +55,9 @@ def process_base_image_sift():
     base_key_points_rich_img = base_gray_small
     base_key_points_rich_img = cv2.drawKeypoints(base_gray_small, key_points, base_key_points_rich_img,
                                                  flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    cv2.imshow("Base with key points", base_key_points_img)
-    cv2.imwrite(OUTPUT_DIRECTORY_ADA + "base_key_points.jpg", base_key_points_img)
-    cv2.imwrite(OUTPUT_DIRECTORY_ADA + "base_key_points_rich.jpg", base_key_points_rich_img)
+    cv2.imshow("Base with key points " + student.upper() + " " + algo_name.upper(), base_key_points_img)
+    cv2.imwrite(OUTPUT_DIRECTORY + student + "/" + algo_name + "/base_key_points.jpg", base_key_points_img)
+    cv2.imwrite(OUTPUT_DIRECTORY + student + "/" + algo_name + "/base_key_points_rich.jpg", base_key_points_rich_img)
 
     return base_gray_small, key_points, descriptor
 
@@ -64,18 +73,17 @@ def detect_object(query_kp, train_kp, matches):
         return M, match_mask
 
     else:
-        return None
+        return None, None
 
 
-def process_test_image_sift(image, name):
+def process_test_image(output_dir, algo, norm, image, name):
     image_gray_small = shrink_image(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))
 
     time_start = time.perf_counter()
-    key_points, descriptor = sift.detectAndCompute(image_gray_small, None)
+    key_points, descriptor = algo.detectAndCompute(image_gray_small, None)
     time_stop = time.perf_counter()
 
-    # I used L1 because they say that "L1 and L2 norms are preferable choices for SIFT and SURF descriptors".
-    bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
+    bf = cv2.BFMatcher(norm, crossCheck=True)
 
     # Query goes first, train goes second. Same with cv2.drawMatches().
     matches = bf.match(descriptor, base_descriptor)
@@ -83,18 +91,24 @@ def process_test_image_sift(image, name):
     metrics.append(Metric(name, image.shape[:2], time_stop - time_start, len(matches)))
 
     M, matches_mask = detect_object(key_points, base_key_points, matches)
+    if M is not None:
+        pts = np.float32([[0, 0], [0, base_height - 1], [base_width - 1, base_height - 1], [base_width - 1, 0]]) \
+            .reshape(-1, 1, 2)
+        dst = cv2.perspectiveTransform(pts, M)
+        moments = cv2.moments(dst)
+        cx, cy = int(moments['m10'] / moments['m00']), int(moments['m01'] / moments['m00'])
 
-    pts = np.float32([[0, 0], [0, base_height - 1], [base_width - 1, base_height - 1], [base_width - 1, 0]]) \
-        .reshape(-1, 1, 2)
-    dst = cv2.perspectiveTransform(pts, M)
+        image_gray_small = cv2.cvtColor(image_gray_small, cv2.COLOR_GRAY2BGR)
+        cv2.circle(image_gray_small, (cx, cy), 15, COLOR_MAGENTA, -1)
 
-    image_gray_small = cv2.polylines(image_gray_small, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
+        image_gray_small = cv2.polylines(image_gray_small, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
 
     match_img = cv2.drawMatches(image_gray_small, key_points, base_image, base_key_points, matches, None,
                                 matchesMask=matches_mask, flags=2)
+
     if random.uniform(0, 1) < IMAGE_SHOW_SAMPLING:
         cv2.imshow(name, match_img)
-    cv2.imwrite(OUTPUT_DIRECTORY_ADA + "match_" + name, match_img)
+    cv2.imwrite(output_dir + "match_" + name, match_img)
 
 
 def save_metrics(directory):
@@ -107,16 +121,32 @@ def save_metrics(directory):
 if __name__ == '__main__':
     cv2.startWindowThread()
     sift = cv2.xfeatures2d.SIFT_create()
+    brisk = cv2.BRISK_create()
 
-    (base_image, base_key_points, base_descriptor) = process_base_image_sift()
-    (base_height, base_width) = base_image.shape[:2]
+    for student_name in ("ada", "vlada"):
+        #  SIFT
+        (base_image, base_key_points, base_descriptor) = process_base_image(student_name, "sift", sift)
+        (base_height, base_width) = base_image.shape[:2]
 
-    metrics = []
+        metrics = []
 
-    for img_name in os.listdir(DATA_DIRECTORY_ADA):
-        img = cv2.imread(DATA_DIRECTORY_ADA + img_name)
-        process_test_image_sift(img, img_name)
+        for img_name in os.listdir(DATA_DIRECTORY + student_name + "/"):
+            img = cv2.imread(DATA_DIRECTORY + student_name + "/" + img_name)
+            # I used L1 because they say that "L1 and L2 norms are preferable choices for SIFT and SURF descriptors".
+            process_test_image(OUTPUT_DIRECTORY + student_name + "/sift/", sift, cv2.NORM_L1, img, img_name)
 
-    save_metrics(OUTPUT_DIRECTORY_ADA)
+        save_metrics(OUTPUT_DIRECTORY + student_name + "/sift/")
+
+        # BRISK
+        (base_image, base_key_points, base_descriptor) = process_base_image(student_name, "brisk", brisk)
+        (base_height, base_width) = base_image.shape[:2]
+
+        metrics = []
+
+        for img_name in os.listdir(DATA_DIRECTORY + student_name + "/"):
+            img = cv2.imread(DATA_DIRECTORY + student_name + "/" + img_name)
+            process_test_image(OUTPUT_DIRECTORY + student_name + "/brisk/", brisk, cv2.NORM_HAMMING, img, img_name)
+
+        save_metrics(OUTPUT_DIRECTORY + student_name + "/brisk/")
 
     cv2.waitKey()